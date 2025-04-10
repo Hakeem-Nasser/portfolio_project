7c344c3d0b02fcaf62a49187d14356e036f801ec
Hakeem Nasser <abdulnasserabdulhakeem@gmail.com>
2025-04-10 09:26:03 +0100

    second commit

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5#)x)8j@w5smji^-ymf(lcz(0whrquc$k1dj#8_r=#*)p9ir3t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio_project.urls'
